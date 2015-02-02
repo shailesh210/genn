b9cac31c9a0eb6c3d552b73e73cde81ef47a436c
esinyavuz <esinyavuzz@gmail.com>
2015-02-02 16:14:04 +0000

    Introducing a copy of the MBody1 example for benchmarking

---
+++ userproject/MBody1_benchmark_project/README.txt
@@ -0,0 +1,50 @@
+
+  Locust olfactory system (Nowotny et al. 2005)
+  (Copy for benchmarking)
+  =============================================
+
+This is a copy of the MBody1 example with the difference that
+- it uses TRAUBMILES_PSTEP as neuron model, and
+- KCDN synapses are generated with the alternative gen_kcdn_syns_fixto10K method in order to
+avoid negative tail of the weight distribution, 
+
+ To compile it, simply type:
+  nmake /f WINmakefile
+for Windows users, or:
+  make
+for Linux, Mac and other UNIX users. 
+
+
+  USAGE
+  -----
+
+  ./generate_run <0(CPU)/1(GPU)> <nAL> <nKC> <nLH> <nDN> <gscale> <DIR> <MODEL> <DEBUG 0(OFF)/1(ON)> <ftype DOUBLE/FLOAT> <reuse input&connectivity? 0(no)/1(yes)>
+
+An example invocation of generate_run is:
+
+  ./generate_run 1 100 1000 20 100 0.0025 outname MBody1 0 FLOAT 0
+
+Such a command would generate a locust olfaction model with 100 antennal lobe neurons,
+1000 mushroom body Kenyon cells, 20 lateral horn interneurons and 100 mushroom body
+output neurons, and launch a simulation of it on a CUDA-enabled GPU using single precision floating point numbers. 
+All output files will be prefixed with "outname" and will be created under the "outname" directory.
+
+In more details, what generate_run program does is: 
+a) use some other tools to generate the appropriate connectivity
+   matrices and store them in files.
+
+b) build the source code for the model by writing neuron numbers into
+   userproject/include/sizes.h, and executing "buildmodel.sh MBody1 [DEBUG OFF/ON]".  
+
+c) compile the generated code by invoking "make clean && make" 
+   running the code, e.g. "./classol_sim r1 1".
+
+
+  MODEL INFORMATION
+  -----------------
+
+For information regarding the locust olfaction model implemented in this example project, see:
+
+T. Nowotny, R. Huerta, H. D. I. Abarbanel, and M. I. Rabinovich Self-organization in the
+olfactory system: One shot odor recognition in insects, Biol Cyber, 93 (6): 436-446 (2005),
+doi:10.1007/s00422-005-0019-7 
---
+++ userproject/MBody1_benchmark_project/model/classol_sim.cu
@@ -0,0 +1,338 @@
+/*--------------------------------------------------------------------------
+   Author: Thomas Nowotny
+  
+   Institute: Institute for Nonlinear Science
+              University of California San Diego
+              La Jolla, CA 92093-0402
+  
+   email to:  tnowotny@ucsd.edu
+  
+   initial version: 2002-09-26
+  
+--------------------------------------------------------------------------*/
+
+//--------------------------------------------------------------------------
+/*! \file classol_sim.cu
+
+\brief Main entry point for the classol (CLASSification in OLfaction) model simulation. Provided as a part of the complete example of simulating the MBody1 mushroom body model. 
+*/
+//--------------------------------------------------------------------------
+
+
+#include "classol_sim.h"
+
+//--------------------------------------------------------------------------
+/*! \brief This function is the entry point for running the simulation of the MBody1 model network.
+*/
+//--------------------------------------------------------------------------
+
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
+    fprintf(stderr, "usage: classol_sim <basename> <CPU=0, GPU=1> \n");
+    return 1;
+  }
+  int GPUarg= atoi(argv[2]);
+  string OutDir = toString(argv[1]) +"_output";
+  string name;
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".time");
+  FILE *timef= fopen(name.c_str(),"a");  
+
+  int which;
+  if (GPUarg > 1) {
+     which= 1;
+     nGPU= GPUarg-2;
+  }    
+  else {
+     which= GPUarg;	
+     nGPU= AUTODEVICE;
+  }
+  patSetTime= (int) (PAT_TIME/DT);
+  patFireTime= (int) (PATFTIME/DT);
+  fprintf(stdout, "# DT %f \n", DT);
+  fprintf(stdout, "# T_REPORT_TME %f \n", T_REPORT_TME);
+  fprintf(stdout, "# SYN_OUT_TME %f \n",  SYN_OUT_TME);
+  fprintf(stdout, "# PATFTIME %f \n", PATFTIME); 
+  fprintf(stdout, "# patFireTime %d \n", patFireTime);
+  fprintf(stdout, "# PAT_TIME %f \n", PAT_TIME);
+  fprintf(stdout, "# patSetTime %d \n", patSetTime);
+  fprintf(stdout, "# TOTAL_TME %f \n", TOTAL_TME);
+  
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".out.Vm"); 
+  FILE *osf= fopen(name.c_str(),"w");
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".out.st"); 
+  FILE *osf2= fopen(name.c_str(),"w");
+
+/*/*!!!!!!
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".gKCDNspiked"); 
+  FILE *out_g= fopen(name.c_str(),"w");
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".inSynKCDNspiked"); 
+  FILE *out_insyn= fopen(name.c_str(),"w");
+//!!!!!!*/
+
+#ifdef TIMING
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".timingprofile"); 
+  FILE *timeros= fopen(name.c_str(),"w");
+  sdkCreateTimer(&timer_gen);
+  double tme;
+#endif
+
+  //-----------------------------------------------------------------
+  // build the neuronal circuitery
+  classol locust;
+
+#ifdef TIMING
+  sdkStartTimer(&timer_gen);
+#endif
+
+  fprintf(stdout, "# reading PN-KC synapses ... \n");
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".pnkc");
+  FILE *f= fopen(name.c_str(),"rb");
+  locust.read_pnkcsyns(f);
+  fclose(f);
+
+#ifdef TIMING
+  sdkStopTimer(&timer_gen);
+  tme= sdkGetTimerValue(&timer_gen);
+  fprintf(timeros, "%% Reading PN-KC synapses: %f \n", tme);
+  sdkResetTimer(&timer_gen);
+  sdkStartTimer(&timer_gen);
+#endif
+
+  fprintf(stdout, "# reading PN-LHI synapses ... \n");
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".pnlhi");
+  f= fopen(name.c_str(), "rb");
+  locust.read_pnlhisyns(f);
+  fclose(f);   
+
+#ifdef TIMING
+  sdkStopTimer(&timer_gen);
+  tme= sdkGetTimerValue(&timer_gen);
+  fprintf(timeros, "%% Reading PN-LHI synapses: %f \n", tme);
+  sdkResetTimer(&timer_gen);
+  sdkStartTimer(&timer_gen);
+#endif
+  
+  fprintf(stdout, "# reading KC-DN synapses ... \n");
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".kcdn");
+  f= fopen(name.c_str(), "rb");
+  locust.read_kcdnsyns(f);
+
+#ifdef TIMING
+  sdkStopTimer(&timer_gen);
+  tme= sdkGetTimerValue(&timer_gen);
+  fprintf(timeros, "%% Reading KC-DN synapses: %f \n", tme);
+  sdkResetTimer(&timer_gen);
+  sdkStartTimer(&timer_gen);
+#endif
+
+  fprintf(stdout, "# reading input patterns ... \n");
+  name= OutDir+ "/"+ toString(argv[1]) + toString(".inpat");
+  f= fopen(name.c_str(), "rb");
+  locust.read_input_patterns(f);
+  fclose(f);
+
+#ifdef TIMING
+  sdkStopTimer(&timer_gen);
+  tme= sdkGetTimerValue(&timer_gen);
+  fprintf(timeros, "%% Reading input patterns: %f \n", tme);
+  sdkResetTimer(&timer_gen);
+  sdkStartTimer(&timer_gen);
+#endif
+
+  locust.generate_baserates();
+  if (which == GPU) {
+    locust.allocate_device_mem_patterns();
+  }
+  locust.init(which);         // this includes copying g's for the GPU version
+
+#ifdef TIMING
+  sdkStopTimer(&timer_gen);
+  tme= sdkGetTimerValue(&timer_gen);
+  fprintf(timeros, "%% Initialisation: %f \n", tme);
+  sdkResetTimer(&timer_gen);
+#endif
+
+  fprintf(stdout, "# neuronal circuitery built, start computation ... \n\n");
+
+  //------------------------------------------------------------------
+  // output general parameters to output file and start the simulation
+
+  fprintf(stdout, "# We are running with fixed time step %f \n", DT);
+  fprintf(stdout, "# initial wait time execution ... \n");
+
+  t= 0.0;
+  int done= 0;
+  float last_t_report=  t;
+  timer.startTimer();
+//  int spkcntr = 0;
+  
+  if (which == GPU){   
+    locust.runGPU(DT);
+    //float synwriteT= 0.0f;
+    //float lastsynwrite= 0.0f;
+    //int synwrite= 0;
+    while (!done) 
+    {    
+      locust.getSpikeNumbersFromGPU();
+      locust.getSpikesFromGPU();
+      locust.runGPU(DT); // run next batch
+    
+    
+#ifdef TIMING
+	fprintf(timeros, "%f %f %f \n", neuron_tme, synapse_tme, learning_tme);
+#endif
+      locust.sum_spikes();
+      locust.output_spikes(osf2, which);
+
+ /*     if ((glbSpkCntDN[0] > 0) && ((t < 500) || (t > TOTAL_TME - 1000))){
+        pullKCDNFromDevice();
+        fprintf(out_insyn,"t=%f ", t);
+        if (spkcntr%10 == 0){
+          fprintf(stdout,"OK! spkcntr = %d", spkcntr );
+          for (int i_kc = 0; i_kc < locust.model.neuronN[1]; i_kc++){ //
+            //fprintf(out_g,"t=%f,i_kc=%d ", t, i_kc);
+            for (int i_dn = 0; i_dn < locust.model.neuronN[3]; i_dn++){
+              fprintf(out_g,"%f ", gKCDN[i_kc*locust.model.neuronN[3]+i_dn]);
+            }
+          }
+        fprintf(out_g,"\n"); 
+        }
+        for (int i_dn = 0; i_dn < locust.model.neuronN[3]; i_dn++){
+          fprintf(out_insyn,"%f ", inSynKCDN[i_dn]);
+        }
+        
+        fprintf(out_insyn,"\n");
+        
+        fprintf(stdout,"%d: %d DN spikes detected at t=%f conductance and insyn written in file \n", spkcntr, glbSpkCntDN[0], t);
+        spkcntr++;
+      }
+	pullDNStateFromDevice();
+  fprintf(osf, "%f ", t);
+  for (int i= 0; i < 100; i++) {
+    fprintf(osf, "%f ", VDN[i]);
+  }
+  fprintf(osf,"\n");
+*/
+    // report progress
+    if (t - last_t_report >= T_REPORT_TME)
+    {
+      fprintf(stdout, "time %f \n", t);
+      last_t_report= t;
+    }
+    // output synapses occasionally
+    /*if (synwrite) {
+       lastsynwrite= synwriteT;
+       name= OutDir+ "/"+ tS(argv[1]) + tS(".") + tS((int) synwriteT) + tS(".syn"); 
+       f= fopen(name.c_str(),"w");
+       locust.write_kcdnsyns(f);
+       fclose(f);
+       synwrite= 0;
+    }
+    if (t - lastsynwrite >= SYN_OUT_TME) {
+       locust.get_kcdnsyns();
+       synwrite= 1;
+       synwriteT= t;
+    }*/
+    done= (t >= TOTAL_TME);
+  }
+}
+  
+  if (which == CPU){   
+    locust.runCPU(DT);
+    //float synwriteT= 0.0f;
+    //float lastsynwrite= 0.0f;
+    //int synwrite= 0;
+    while (!done) 
+    {
+
+      locust.runCPU(DT); // run next batch
+    // if (which == GPU) {  
+//	pullDNStateFromDevice();
+    //   }
+    
+#ifdef TIMING
+	if (which == CPU) {
+	    fprintf(timeros, "%f %f %f \n", sdkGetTimerValue(&neuron_timer), sdkGetTimerValue(&synapse_timer), sdkGetTimerValue(&learning_timer));
+	}
+	else {
+	    fprintf(timeros, "%f %f %f \n", neuron_tme, synapse_tme, learning_tme);
+	}
+#endif
+
+      locust.sum_spikes();
+      locust.output_spikes(osf2, which);
+      
+ /*     if ((glbSpkCntDN[0] > 0) && ((t < 500) || (t > TOTAL_TME - 1000))){
+        fprintf(out_insyn,"t=%f ", t);
+        if (spkcntr%10 == 0){
+          fprintf(stdout,"OK! spkcntr = %d", spkcntr );
+          for (int i_kc = 0; i_kc < locust.model.neuronN[1]; i_kc++){ //
+            //fprintf(out_g,"t=%f,i_kc=%d ", t, i_kc);
+            for (int i_dn = 0; i_dn < locust.model.neuronN[3]; i_dn++){
+              fprintf(out_g,"%f ", gKCDN[i_kc*locust.model.neuronN[3]+i_dn]);
+            }
+          }
+        fprintf(out_g,"\n"); 
+        }
+        for (int i_dn = 0; i_dn < locust.model.neuronN[3]; i_dn++){
+          fprintf(out_insyn,"%f ", inSynKCDN[i_dn]);
+        }
+        
+        fprintf(out_insyn,"\n");
+        
+        fprintf(stdout,"%d: %d DN spikes detected at t=%f conductance and insyn written in file \n", spkcntr, glbSpkCntDN[0], t);
+        spkcntr++;
+      }
+ /*   fprintf(osf, "%f ", t);
+    //  for (int i= 0; i < 100; i++) {
+    //     fprintf(osf, "%f ", VDN[i]);
+    //   }
+    // fprintf(osf,"\n");
+*/
+    // report progress
+    if (t - last_t_report >= T_REPORT_TME)
+    {
+      fprintf(stdout, "time %f \n", t);
+      last_t_report= t;
+    }
+    // output synapses occasionally
+    /*if (synwrite) {
+       lastsynwrite= synwriteT;
+       name= OutDir+ "/"+ tS(argv[1]) + tS(".") + tS((int) synwriteT) + tS(".syn"); 
+       f= fopen(name.c_str(),"w");
+       locust.write_kcdnsyns(f);
+       fclose(f);
+       synwrite= 0;
+    }
+    if (t - lastsynwrite >= SYN_OUT_TME) {
+       locust.get_kcdnsyns();
+       synwrite= 1;
+       synwriteT= t;
+    }*/
+    done= (t >= TOTAL_TME);
+  }
+}
+  timer.stopTimer();
+  pullDNStateFromDevice();
+  cerr << "output files are created under the current directory." << endl;
+  fprintf(timef, "%d %u %u %u %u %u %.4f %.2f %.1f %.2f\n",which, locust.model.sumNeuronN[locust.model.neuronGrpN-1], locust.sumPN, locust.sumKC, locust.sumLHI, locust.sumDN, timer.getElapsedTime(),VDN[0], TOTAL_TME, DT);
+fprintf(stdout, "GPU=%d, %u neurons, %u PN spikes, %u KC spikes, %u LHI spikes, %u DN spikes, simulation took %.4f secs, VDN[0]=%.2f DT=%.1f %.2f\n",which, locust.model.sumNeuronN[locust.model.neuronGrpN-1], locust.sumPN, locust.sumKC, locust.sumLHI, locust.sumDN, timer.getElapsedTime(),VDN[0], TOTAL_TME, DT);
+  fclose(osf);
+  fclose(osf2);
+//  fclose(out_g);
+ // fclose(out_insyn);
+  fclose(timef);
+
+#ifdef TIMING
+  fclose(timeros);
+#endif
+
+  if (which == GPU) {
+    locust.free_device_mem();
+  }
+  
+  return 0;
+}
